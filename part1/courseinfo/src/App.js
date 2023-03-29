5f99fdb64e5b63a5c5f9cd6c1a309eda36b6ac6e
Brendan Carroll <brendan.carroll@workhuman.com>
2023-03-29 18:09:40 +0100

    Completing exercises 1.3 to 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,49 +1,64 @@
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h1>{course.name}</h1>;
 
-const Total = ({ total }) => <p>Number of exercises {total}</p>;
+const Total = ({ course }) => {
+  const { parts } = course;
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts.reduce(
+        (accumulator, currentPart) => accumulator + currentPart.exercises,
+        0
+      )}
+    </p>
+  );
+};
 
-const Content = ({
-  part1,
-  exercises1,
-  part2,
-  exercises2,
-  part3,
-  exercises3,
-}) => (
-  <div>
-    <Part part={part1} exercise={exercises1} />
-    <Part part={part2} exercise={exercises2} />
-    <Part part={part3} exercise={exercises3} />
-  </div>
-);
-
-const Part = ({ part, exercise }) => (
-  <p>
-    {part} {exercise}
-  </p>
-);
-
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
+const Content = ({ course }) => {
+  const { parts } = course;
   return (
     <div>
+      {/* Follow KISS principle: There's only ever going to be 3 parts so code approps */}
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    // Possible best practice: Pass same object if possible
+    <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
