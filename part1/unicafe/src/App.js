473970683772c100439d11bbe419c7693e2fd1a4
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-05 12:28:43 +0100

    complete exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,27 @@ const Button = ({ text, handleClick }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Statistics = (props) => {
+  const {
+    goodCounter,
+    neutralCounter,
+    badCounter,
+    total,
+    average,
+    positivePercentage,
+  } = props;
+  return (
+    <div id="statistics">
+      <p>good {goodCounter}</p>
+      <p>neutralCounter {neutralCounter}</p>
+      <p>badCounter {badCounter}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positivePercentage {positivePercentage}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [goodCounter, setGoodCounter] = useState(0);
   const [neutralCounter, setNeutralCounter] = useState(0);
@@ -42,14 +63,14 @@ const App = () => {
         <Button text={"neutral"} handleClick={updateCounter("neutral")} />
         <Button text={"bad"} handleClick={updateCounter("bad")} />
       </div>
-      <div id="statistics">
-        <p>good {goodCounter}</p>
-        <p>neutralCounter {neutralCounter}</p>
-        <p>badCounter {badCounter}</p>
-        <p>all {total}</p>
-        <p>average {average}</p>
-        <p>positivePercentage {positivePercentage}%</p>
-      </div>
+      <Statistics
+        goodCounter={goodCounter}
+        neutralCounter={neutralCounter}
+        badCounter={badCounter}
+        total={total}
+        average={average}
+        positivePercentage={positivePercentage}
+      />
     </div>
   );
 };
