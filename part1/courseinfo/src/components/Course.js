7473fcdcc270c579601ffa9f19d4d5aa0f7e4ca0
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-05 15:51:29 +0100

    complete exercise 2.5

---
+++ part1/courseinfo/src/components/Course.js
@@ -0,0 +1,48 @@
+const Header = ({ course }) => <h1>{course.name}</h1>;
+
+const Total = ({ course }) => {
+  const { parts } = course;
+  return (
+    <p>
+      <b>
+        Number of exercises{" "}
+        {parts.reduce(
+          (currentSum, currentPart) => currentSum + currentPart.exercises,
+          0
+        )}
+      </b>
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  const { parts } = course;
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+export default Course;
