1dc1989595b88ab2b33b21bf5d9f434af4417a16
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-05 17:04:36 +0100

    adding effect hooks code + adding axios and json server to phonebook

--- part2/follow-along/src/App.js
+++ part2/follow-along/src/App.js
@@ -1,12 +1,24 @@
 import Note from "./components/Note";
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note..."); //to make this controlled by the app, **we** need to handle the state changes
   const [showAll, setShowAll] = useState(true);
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
