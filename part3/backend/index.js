67ca27868cd2ee55f00a5a3d794a966d12ba28b2
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-06 18:10:13 +0100

    saving backend follow-along

---
+++ part3/backend/index.js
@@ -0,0 +1,78 @@
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+
+app.use(express.json()); // allows express to parse json payloads
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (_request, response) => {
+  response.send("<h1>Hello, World</h1>");
+});
+
+app.get("/api/notes", (_request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const { body } = request;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
