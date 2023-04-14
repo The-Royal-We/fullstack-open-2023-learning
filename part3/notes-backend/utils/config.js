738169a4193919b26c0d990440911fc98a78ff5e
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-14 11:02:58 +0100

    refactoring config.js

--- part3/notes-backend/utils/config.js
+++ part3/notes-backend/utils/config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const { PORT, MONGODB_URI } = process.env;
 
 module.exports = {
   MONGODB_URI,
