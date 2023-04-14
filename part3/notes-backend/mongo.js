787d02dc799a8737210bd79b93cee2edd6d256ef
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-14 11:12:58 +0100

    adding tests to note_api

--- part3/notes-backend/mongo.js
+++ part3/notes-backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.1iijure.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.1iijure.mongodb.net/testNoteApp?retryWrites=true&w=majority`;
 
 mongoose.set('strictQuery', false);
 mongoose.connect(url);
